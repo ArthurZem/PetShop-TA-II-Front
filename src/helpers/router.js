d8ac62997e03dde5be52475ad465c4402160031a
Arthur Zem de Souza <arthurzem1612@gmail.com>
2023-09-12 16:48:18 -0300

    login + awth + fake backend

---
+++ src/helpers/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import { useAuthStore } from '@/stores';
+import { HomeView, LoginView } from '@/views';
+
+export const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    linkActiveClass: 'active',
+    routes: [
+        { path: '/', component: HomeView },
+        { path: '/login', component: LoginView }    ]
+});
+
+router.beforeEach(async (to) => {
+    const publicPages = ['/login'];
+    const authRequired = !publicPages.includes(to.path);
+    const auth = useAuthStore();
+
+    if (authRequired && !auth.user) {
+        auth.returnUrl = to.fullPath;
+        return '/login';
+    }
+});
